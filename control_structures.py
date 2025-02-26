a24af68cf8129525f937735cf97cccfc99d0d77f
Nevean <nevadhis@gmail.com>
2025-02-26 20:44:28 +0300

    Folder showing python basics

---
+++ control_structures.py
@@ -0,0 +1,19 @@
+def classify_number(a):
+    if a % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+#Ask user for an integer and classify the number as even or odd    
+num = int(input("Enter a number: "))
+print(f"The number {num} is {classify_number(num)}")
+
+#Generate list of even numbers between 1 and 50
+even_numbers = [i for i in range(1, 51) if i % 2 == 0]
+print(even_numbers)
+
+#Print numbers from 10 to 1 in reverse order using a while loop
+count = 10
+while count > 0:
+    print(count)
+    count -= 1
---
+++ datatypes.py
@@ -0,0 +1,29 @@
+#Define variables of each data type
+int_a = 500
+float_b =  89.56
+complex_c = 7+3j
+list1 = [1,2,3,4,5]
+tuple1 = (14,15,16,17,18)
+dict_x = {"Jane": 22, "Abel": 30, "Whitney": 27, "Cain": 24}
+set_y = {7,8,9,10,11}
+bool1 = True
+
+#Print the type of each data type
+print("Type of int_a is: ",type(int_a))
+print("Type of float_b is: ",type(float_b))
+print("Type of complex_c is: ",type(complex_c))
+print("Type of list1 is: ",type(list1))
+print("Type of tuple1 is: ",type(tuple1))
+print("Type of dict_x is: ",type(dict_x))
+print("Type of set_y is: ",type(set_y))
+print("Type of bool1 is: ",type(bool1))
+
+#Convert integer to float
+converted_float = float(int_a)
+print(converted_float)
+ 
+#Convert float to integer
+converted_int = int(float_b)
+print(converted_int)
+
+
